19f5cd7d2225c50dd0678f5c59b7caa1bce697bc
David Russell <russeldj@clarkson.edu>
2020-04-23 10:27:02 -0400

    Working assignment 2

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.random import normal
+import pandas as pd
 import pdb
+import matplotlib.pyplot as plt
 
 
 class neuron():
@@ -84,7 +86,8 @@ class NN():
         layers = []
         for i in range(len(sizes) - 2):
             layers.append(layer(sizes[i+1], sizes[i]))
-        layers.append(layer(sizes[-1], sizes[-2], 'sigmoid'))
+        # since it's classifcation with [-1, 1]
+        layers.append(layer(sizes[-1], sizes[-2], 'tanh'))
         return layers
 
     def forward(self, x):
@@ -135,7 +138,7 @@ class NN():
 
         sigmas = [None] * len(ys)  # fill each element with None
         nonlinear_prime = self.layers[-1].nonlinearity_prime(ys[-1])
-        error = (label - xs[-1])
+        error = (xs[-1] - label)
         sigma = error * nonlinear_prime
         sigmas[-1] = sigma
         for i in range(len(ys) - 2, -1, -1):
@@ -157,22 +160,83 @@ class NN():
             # compute the gradient matrix
             grad_matrix = np.matmul(sigma_transpose, x)
             if grad_matrix.shape != self.layers[i].weights.shape:
-                raise AssertionError("Something is wrong")
-            self.layers[i].weights += alpha * \
-                grad_matrix  # why is this positive
+                raise AssertionError(
+                    "gradients are not the same size as the weights")
+            # perform the update
+            self.layers[i].weights -= alpha * grad_matrix
 
-    def train(self, features, labels, num_iterations=100):
+    def train(self, features, labels, num_iterations=100, alpha=0.001, vis=False):
+        all_losses = []
         for iteration in range(num_iterations):
+            loss = 0
             for (feature, label) in zip(features, labels):
                 pred, xs, ys = self.forward(feature)
                 if len(pred) != 1:
                     raise Exception(
                         "The length of the prediction was expected to be one")
-                loss = 1/2.0*(pred[0] - label)**2
-                print("iteration : {}, y_pred : {}, y : {}, loss : {}".format(
-                    iteration, pred, label, loss))
-                self.compute_matrix_backprop(label, xs, ys)
+                loss += 1/2.0*(pred[0] - label)**2
+                self.compute_matrix_backprop(label, xs, ys, alpha)
+            average_loss = loss / num_iterations
+            all_losses.append(average_loss)
+            print("iteration : {}, average loss : {}".format(
+                iteration, average_loss))
+
+            if vis:
+                plt.scatter(iteration, average_loss, c='b')
+                plt.pause(0.1)
+        plt.plot(all_losses)
+
+    def score(self, features, labels):
+        """
+        assumes that labels are [-1, 1]
+        """
+        preds = self.pred(features)
+        preds = np.squeeze(np.asarray(preds))
+        preds = (preds > 0).astype(int) * 2 - 1
+        equal = preds == labels
+        accuracy = np.sum(equal) / equal.shape[0]
+        print("The accuracy was {}".format(accuracy))
+
+        pdb.set_trace()
+
+    def pred(self, features):
+        preds = []
+        for feature in features:
+            pred, _, _ = self.forward(feature)
+            preds.append(pred)
+        return preds
+
+    def write_out(self, filename="output.txt"):
+        with open(filename, "w") as outfile:
+            num_hidden = self.layers[0].weights.shape[0]
+            outfile.write("{}\n".format(num_hidden))
+            print("Writing weights to {}".format(filename))
+            print("The shape of the output layer is {} and the input weights are {}".format(
+                self.layers[1].weights.shape, self.layers[0].weights.shape))
+            np.savetxt(
+                outfile,
+                self.layers[1].weights,
+                fmt="%.5f",
+                delimiter=" ")
+            np.savetxt(
+                outfile,
+                self.layers[0].weights,
+                fmt="%.5f",
+                delimiter=" ")
 
 
-net = NN([3, 5, 8, 1])
-net.train([[2, 2, 2], [0, 1, 2]], [0.5, 0.1])
+def read_data(values_file, labels_file):
+    features = pd.read_csv(values_file, sep=' ',
+                           header=None)  # read in the data
+    features = features.dropna(axis=1).values  # drop the nan column
+    labels = pd.read_csv(labels_file, sep=' ', header=None)
+    labels = np.squeeze(labels.values)
+    return features, labels
+
+
+(features, labels) = read_data(u"/home/david/dev/ClarksonCompLearning/data/train_data.txt",
+                               u"/home/david/dev/ClarksonCompLearning/data/train_label.txt")
+net = NN([1000, 200, 1])
+net.train(features, labels, 2000, 0.001)
+net.score(features, labels)
+net.write_out()
